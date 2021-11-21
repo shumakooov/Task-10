c42d1021470ea83945440bd278294490357ba97a
shumakooov <shumakov.13@yandex.ru>
2021-11-21 18:11:58 +0500

    docstring

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -3,6 +3,14 @@ import numpy as np
 
 
 def get_mosaic(image, size, gradation):
+    '''
+    Принимает изображение и применяет к нему фильтр, который делает черно-белый пиксель-арт
+    :param image: исходное изображение
+    :param size: размер мозайки
+    :param gradation: размер градации
+    :return: новое изображение(пиксль-арт)
+
+    '''
     image_arr = np.array(Image.open(image)).astype(int)
     limit = 255 // gradation
     image_len = len(image_arr)
@@ -21,10 +29,18 @@ def get_mosaic(image, size, gradation):
 
 
 def set_color(new_c, matrix, size, i, j):
+    '''
+    Принимает размер мозайки и устанавливает новый цвет для пикселя
+    :param new_c: новый цвет пикселя
+    :param matrix: матрица изображения
+    :param size: размер мозайки
+    :param i: пиксель по оси x
+    :param j: пиксель по оси y
+    :return: устанавливает новый цвет пикселя
+    '''
     for x in range(i, i + size):
         for y in range(j, j + size):
             for z in range(3):
                 matrix[x][y][z] = new_c
 
-
-get_mosaic('img.jpg', 10, 50).save('res.jpg')
+get_mosaic('img1.jpg', 10, 50).save('res.jpg')
